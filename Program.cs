919334bcf1098c8b18ff448f761c77e04073ffc4
Nicolas <nespinoza.naer@gmail.com>
2023-10-11 18:14:14 -0500

    feat: added MySql connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
+using LearningCenter.API.Learning.Domain.Repositories;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Mapping;
+using LearningCenter.API.Learning.Services;
+using LearningCenter.API.Shared.Persistence.Contexts;
+using LearningCenter.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Database Connection
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+// Add lowercase routes
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 // Add services to the container.
 
@@ -9,8 +31,28 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Validation for ensuring Database Objects are created
+
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+    context.Database.EnsureCreated();
+}
+
+// AutoMapper Configuration
+
+builder.Services.AddAutoMapper(
+    typeof(ModelToResourceProfile),
+    typeof(ResourceToModelProfile));
+
+// Dependency Injection Configuration
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+    if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
